2d8a5094563beaabed023af64baceff9447b4bb7
kojolako <kojolako@gmail.com>
2018-10-24 19:32:45 +0400

    update telegram-api

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import requests
 
-url = "https://api.telegram.org/bot(TOKEN)/"
-
+url = "https://api.telegram.org/bot"
+TOKEN_TELEGRAM = "0"
 
 def get_updates_json(request):  
-    response = requests.get(request + 'getUpdates')
+    response = requests.get(request + TOKEN_TELEGRAM + '/getUpdates')
     return response.json()
 
 
@@ -12,6 +12,8 @@ def last_update(data):
     results = data['result']
     total_updates = len(results) - 1
     return results[total_updates]
+
+TOKEN_TELEGRAM = input("TOKEN")
 
 upd = open("upd.txt", "w")
 
