248975764321289b7f460cdcccea366530dbc46c
kojolako <kojolako@gmail.com>
2018-10-29 22:11:09 +0400

    update

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import telegram
+from tkinter import *
+
 
 
 def main():
     telegram.writetofilelastmassages("LASTDATA")
+    root = Tk()
+    root.title("Графическая программа на Python")
+    root.geometry("400x300")
+
+    root.mainloop()
 
 main()
 
